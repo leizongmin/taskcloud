3009db28898038dec2f27a318f7d222fdb661ded
Lei Zongmin <leizongmin@gmail.com>
2011-09-17 17:22:34 +0800

    启动服务器时，自动载入快照，并按预设的周期自动保存快照

--- bin/server.js
+++ bin/server.js
@@ -6,39 +6,52 @@
  */
  
 var web = require('QuickWeb');
+
 var taskvm = require('../lib/taskvm');
 var logcache = require('../lib/logcache');
+var snapshot = require('../lib/snapshot');
 
 var server = module.exports;
 
 /**
  * 启动一个HTTP服务实例
  *
- * @param {string} template_dir 任务模版目录
- * @param {string} server_token 授权码，用于验证客户端的身份
- * @param {int} queue_cycle 任务扫描周期
  * @param {int} server_port 服务器端口
+ * @param {string} template_dir 任务模版目录
+ * @param {int} queue_cycle 任务扫描周期
+ * @param {int} snapshot_cycle 快照保存周期
+ * @param {int} snapshot_path 快照保存路径
  * @param {int} time_zone 时区 例：+8
  */
-server.run = function (template_dir, server_token, queue_cycle, server_port, time_zone) {
+server.run = function (server_port, template_dir, queue_cycle, snapshot_cycle, snapshot_path, time_zone) {
 	/* 默认参数 */
-	if (!template_dir)
-		template_dir = './task_templates';	// 默认模版目录
-	if (typeof queue_cycle == 'undefined' || queue_cycle < 100)
-		queue_cycle = 1000;			// 默认扫描周期为1s
+	// 默认端口为8080
 	if (typeof server_port == 'undefined' || server_port < 1)
-		server_port = 8080;			// 默认端口为8080
+		server_port = 8080;
+	// 默认模板目录
+	if (!template_dir)
+		template_dir = './task_templates';
+	// 默认扫描周期为1秒, 不能小于1秒
+	if (typeof queue_cycle == 'undefined' || queue_cycle < 100)
+		queue_cycle = 1000;
+	// 默认快照周期为10分钟, 不能小于10秒
+	if (typeof snapshot_cycle == 'undefined' || snapshot_cycle < 10000)
+		snapshot_cycle = 600000;
+	// 默认快照保存路径
+	if (!snapshot_path)
+		snapshot_path = './snapshot.json';
+	// 默认时区为+8
 	if (isNaN(time_zone))
-		time_zone = 8;				// 默认时区为+8
-	if (!server_token)
-		server_token = '';			// 默认授权码为空
+		time_zone = 8;
 		
-	console.log('Server config:');
-	console.log('template_dir=' + template_dir);
-	console.log('server_token=' + server_token);
-	console.log('queue_cycle=' + queue_cycle);
-	console.log('server_port=' + server_port);
-	console.log('time_zone=' + time_zone);
+	console.log('----------------------  Server config  ----------------------');
+	console.log('server_port = ' + server_port);
+	console.log('template_dir = ' + template_dir);
+	console.log('queue_cycle = ' + queue_cycle);
+	console.log('snapshot_cycle = ' + snapshot_cycle);
+	console.log('snapshot_path = ' + snapshot_path);
+	console.log('time_zone = ' + time_zone);
+	console.log('-------------------------------------------------------------');
 	
 	//--------------------------初始化日志记录---------------------------------------------
 	var debug = logger = function (msg) {
@@ -63,6 +76,17 @@ server.run = function (template_dir, server_token, queue_cycle, server_port, tim
 	//--------------------------初始化taskvm--------------------------------------------
 	taskvm.init(template_dir, queue_cycle);
 	taskvm.start();
+	
+	//--------------------------初始化snapshot--------------------------------------------
+	snapshot.load(snapshot_path, function (ok) {
+		debug('Load snapshot data ' + (ok ? 'success.' : 'fail!'));
+	});
+	setInterval(function () {
+		snapshot.save(snapshot_path, function (ok) {
+			debug('Save snapshot data ' + (ok ? 'success.' : 'fail!'));
+		});
+	}, snapshot_cycle);
+	
 
 	console.log('+------------------------------------------------------------------+');
 	console.log('+                                                                  +');
--- start.js
+++ start.js
@@ -8,11 +8,12 @@
 var server = require('./bin/server');
 
 // 系统配置
+var SERVER_PORT = 80;							// 服务器端口
 var TEMPLATE_PATH = './task_templates';		// 模板目录
-var SERVER_TOKEN = '';						// 授权码，用于客户端与服务器通讯（暂时不可用）
 var QUEUE_CYCLE = 1000;						// 任务队列扫描周期
-var SERVER_PORT = 80;						// 服务器端口
+var SNAPSHOT_CYCLE = 1000;					// 快照保存周期
+var SNAPSHOT_PATH = './snapshot.json';		// 快照文件名
 var TIME_ZONE = 8;							// 时区，主要用于日志输出中的时间格式
 
 // 启动服务器
-server.run(TEMPLATE_PATH, SERVER_TOKEN, QUEUE_CYCLE, SERVER_PORT, TIME_ZONE);
+server.run(SERVER_PORT, TEMPLATE_PATH, QUEUE_CYCLE, SNAPSHOT_CYCLE, SNAPSHOT_PATH, TIME_ZONE);
